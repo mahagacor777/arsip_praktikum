4c139f9aebbd5ceb80bcc367b395bcb0f1465a3b
mahagacor777 <icalnov755@gmail.com>
2026-02-19 15:15:57 +0700

    Add var_data.py with variable declarations and prints

---
+++ pertemuan1/var_data.py
@@ -0,0 +1,16 @@
+#variabel string 
+nama_kelas= "Struktur data"
+
+#variabel integer
+jumlah_mahasiswa= 3
+
+#variabel float
+nilai_rata= 0.0
+
+#variabel boolean
+kelas_aktif = True 
+print(nama_kelas)
+print (jumlah_mahasiswa)
+print (nilai_rata)
+print (kelas_aktif)
+
