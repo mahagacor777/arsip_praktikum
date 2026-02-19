ac0234795c7c68bdbdaba20aa8272b6f8c316e7a
mahagacor777 <icalnov755@gmail.com>
2026-02-19 15:42:41 +0700

    Create input.py

---
+++ pertemuan1/input.py
@@ -0,0 +1,8 @@
+nama =input("masukkan nama mahasiswa: ")
+umur = int( input("masukkan umur: "))
+ipk  = float (input(masukkan ipk: "))
+
+
+ print ("nama :", nama)
+ print ("umur :", umur)
+ print ("ipk: ", ipk)
