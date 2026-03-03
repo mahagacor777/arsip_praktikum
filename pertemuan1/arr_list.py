7c0f6e664461e65e87c60427a53f0d78bd024121
mahagacor777 <icalnov755@gmail.com>
2026-03-03 10:14:24 +0700

    Update arr_list.py

--- pertemuan1/arr_list.py
+++ pertemuan1/arr_list.py
@@ -18,4 +18,10 @@ print("\n===== DATA NILAI SEMUA KELAS =====")
 
 for k in range (len/(data_nilai))
      print(f"nkelas ke- {kelas+1} ")
+     for i in range (len(data_nilai[k])):
+         print(f" Mahasiswa ke_{i+1}")
+         print("   Tugas:", data_nilai[k][i][0])
+         print("   UTS  :", data_nilai[k][i][1])
+         print("   UAS  :", data_nilai[k][i][2])
+       
   
