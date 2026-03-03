99db09b30e78e8c772aabf4b0bb9108f5b9d6e1c
mahagacor777 <icalnov755@gmail.com>
2026-03-03 09:54:50 +0700

    Create arr_list.py

---
+++ pertemuan1/arr_list.py
@@ -0,0 +1,21 @@
+#MEMBUAT ARRAY KOSONG
+DATA_NILAI= []
+JUMLAH_KELAS = int (input("masukkan jumlah kelas : "))
+
+for i in range (jumlah_kelas):
+  print(f"\nmahasiswa ke- {i+1}")
+
+  tugas= float(input("nilai tugas: "))
+  uts = float(input("nilai uts :  "))
+  uas = float (input ("nilai uas : "))
+  #simpan kelas ke dalam array 2D
+  kelas.append([tugas,uts,uas])
+
+  #simpan kelas ke dalam array 3D
+data_nilai.append(kelas)
+
+print("\n===== DATA NILAI SEMUA KELAS =====")
+
+for k in range (len/(data_nilai))
+     print(f"nkelas ke- {kelas+1} ")
+  
