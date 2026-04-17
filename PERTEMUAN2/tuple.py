215460224f5d8cbb0392b9d51c0e56d75abe31e6
mahagacor777 <icalnov755@gmail.com>
2026-04-17 21:44:54 +0700

    Implement tuple operations in tuple.py
    Add tuple operations and print statements for fruits.

---
+++ PERTEMUAN2/tuple.py
@@ -0,0 +1,14 @@
+data_kosong= ()
+buah= ("apple", "banana", "cherry")
+
+angka_satu=(5,)
+
+print("isi buah", buah)
+
+print("elemen pertama: , buah[0])
+print("elemen terakhir: , buah[-1])
+
+print("jumlah elemen/ panjang set", len(buah))
+
+for item in buah:
+      print("Buah", item)
